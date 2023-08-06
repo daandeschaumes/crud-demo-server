4bcda0777e48642faa9c9ec88e09fbf1ff30e1cd
dandes97 <daan.deschaumes@gmail.com>
2023-08-06 20:44:59 +0200

    disable cors-options - retry

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ import { credentials } from "./middleware/credentials.js";
 
 dotenv.config();
 const app = express();
-app.use(credentials);
-// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(cookieParser());
 
 app.get("", (req, res) => {
   res.send({ message: "Hello World!" });
