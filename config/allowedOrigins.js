918f8391a50c8678e8b84c36fa8a5e16d8793763
dandes97 <daan.deschaumes@gmail.com>
2023-08-06 20:36:27 +0200

    allow netlify origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://64cfe664a3461a702547e828--sparkly-eclair-ce0e46.netlify.app/",
+];
 
 export { allowedOrigins };
